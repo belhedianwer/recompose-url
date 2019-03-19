fcb350025c834d596f48f9a6602e7bbc0b79fb3b
Anwer Awled Belhedi <belhedi.anwer@gmail.com>
2019-03-19 12:31:11 +0100

    Check a valid URL

--- index.js
+++ index.js
@@ -27,26 +27,30 @@ module.exports = {
     var self = this,
         urls = {};
 
+    if (!self.validURL(_url)) {
+      var error = {code: 10, status: 'ko', message: 'setUrls function: Url undefined! | Url value: ' + _url};
+      return callback(error, urls);
+    }
+
     self.setProtocol(_url, callRequest, function (err, result) {
       _url = result;
 
       var parts = url.parse(_url);
 
-      if (parts.host && parts.href) {
-        urls = _.extend(urls, {originUrl: parts.protocol + '//' + parts.host});
+      urls = _.extend(urls, {originUrl: parts.protocol + '//' + parts.host});
 
-        if (!self.hasWww(_url)) {
-          urls = _.extend(urls, {withHttpWww: self.httpProtocol + 'www.' + parts.host});
-          urls = _.extend(urls, {withHttpsWww: self.httpsProtocol + 'www.' + parts.host});
-          urls = _.extend(urls, {withHttp: self.httpProtocol + parts.host});
-          urls = _.extend(urls, {withHttps: self.httpsProtocol + parts.host});
-        } else {
-          urls = _.extend(urls, {withHttpWww: self.httpProtocol + parts.host});
-          urls = _.extend(urls, {withHttpsWww: self.httpsProtocol + parts.host});
-          urls = _.extend(urls, {withHttp: self.httpProtocol + parts.host.replace(/^www\./, '')});
-          urls = _.extend(urls, {withHttps: self.httpsProtocol + parts.host.replace(/^www\./, '')});
-        }
+      if (!self.hasWww(_url)) {
+        urls = _.extend(urls, {withHttpWww: self.httpProtocol + 'www.' + parts.host});
+        urls = _.extend(urls, {withHttpsWww: self.httpsProtocol + 'www.' + parts.host});
+        urls = _.extend(urls, {withHttp: self.httpProtocol + parts.host});
+        urls = _.extend(urls, {withHttps: self.httpsProtocol + parts.host});
+      } else {
+        urls = _.extend(urls, {withHttpWww: self.httpProtocol + parts.host});
+        urls = _.extend(urls, {withHttpsWww: self.httpsProtocol + parts.host});
+        urls = _.extend(urls, {withHttp: self.httpProtocol + parts.host.replace(/^www\./, '')});
+        urls = _.extend(urls, {withHttps: self.httpsProtocol + parts.host.replace(/^www\./, '')});
       }
+
       callback(null, urls);
     });
   },
@@ -63,8 +67,14 @@ module.exports = {
    * 
    */
   setProtocol: function (_url, callRequest = false, callback) {
-    var self = this,
-        _url = (_url === undefined ? '' : _url.trim());
+    var self = this;
+
+    if (!self.validURL(_url)) {
+      var error = {code: 11, status: 'ko', message: 'setProtocol function: Url undefined! | Url value: ' + _url};
+      return callback(error, _url);
+    }
+
+    _url = _url.trim();
 
     // Check if https or http
     if (!self.hasProtocol(_url)) {
@@ -94,8 +104,12 @@ module.exports = {
    *  OUT e.g.,  false | true
    */
   hasProtocol: function (_url) {
-    var self = this,
-        _url = (_url === undefined ? '' : _url.trim());
+    var self = this;
+
+    if (!self.validURL(_url))
+      return false;
+
+    _url = _url.trim();
 
     if (_url.indexOf(self.httpProtocol) == -1 && _url.indexOf(self.httpsProtocol) == -1)
       return false;
@@ -112,10 +126,34 @@ module.exports = {
    *  OUT e.g.,  true | false
    */
   hasWww: function (_url) {
-    _url = (_url === undefined ? '' : _url.trim());
+    var self = this;
+
+    if (!self.validURL(_url))
+      return false;
+
+    _url = _url.trim();
 
     if (_url.indexOf("www") == -1)
       return false;
     return true;
+  },
+  /**
+   * Check if url is valid
+   *
+   * @param {string} _url
+   * @return {boolean} 
+   * 
+   *  IN e.g., function ('www.domain.com', true) | function ('www.domain.', false)
+   *  
+   *  OUT e.g.,  true | false
+   */
+  validURL: function (_url) {
+    var pattern = new RegExp('^(https?:\\/\\/)?' + // protocol
+        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+        '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+        '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+        '(\\#[-a-z\\d_]*)?$', 'i'); // fragment locator
+    return !!pattern.test(_url);
   }
 };
